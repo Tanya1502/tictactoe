9694e0c090e38b96aa5d8978207dc4cc47729012
Tanya <tannutanyaverma@gmail.com>
2023-03-15 18:59:08 -0500

    added title and inline css

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -73,7 +76,7 @@ function App() {
       >
         Start new Game
       </button>
-      <h2> Current Game History</h2>
+      <h2 style={{ fontWeight: 'normal' }}> Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
